3eccceca601358bc89ae7aa01d43304eaafe6fb2
fregnani123 <fabianofregnani@hotmail.com>
2023-07-18 17:19:29 -0300

    estudos

---
+++ src/controllers/carroController.js
@@ -0,0 +1,1 @@
+const carroServices = require('../services/carroServices');
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router;
 
+const carroController = require('./controllers/carroController');
 
 module.exports = router;
