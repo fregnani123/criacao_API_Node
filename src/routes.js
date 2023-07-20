b2d7ab5b05db7c1b07189a9fc6d95cbb9a850725
fregnani123 <fabianofregnani@hotmail.com>
2023-07-20 19:13:49 -0300

    estudos

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 
 const carroController = require('./controllers/carroController');
 
+router.get('/carros', carroController.buscarTodos)
+
 module.exports = router;
 
