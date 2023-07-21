e802190af40af4cd20fcfa44a16536c4eb1e6fe7
fregnani123 <fabianofregnani@hotmail.com>
2023-07-21 20:22:12 -0300

    estudos

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
-require('dotenv').config({path:'variaveis.env'});
+require('dotenv').config({ path: 'variaveis.env' });
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
+
 const routes = require('./routes');
 const server = express();
-
-
 server.use(cors());
-server.use(bodyParser.urlencoded({
-    extended:false
-}));
+server.use(bodyParser.urlencoded({ extended: false }));
 
-server.use('/api',routes);
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-server.listen(process.env.PORT,() => {
-    console.log(`Servidor rodando em: http://localhost:${process.env.PORT}`)
+server.use(express.static(publicDirectoryPath));
+
+server.use('/api', routes);
+
+server.listen(process.env.PORT, () => {
+  console.log(`Servidor rodando em: http://localhost:${process.env.PORT}`);
 });
