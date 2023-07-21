b74149df267e4d9e75c8616d4468e4b95d55fb59
fregnani123 <fabianofregnani@hotmail.com>
2023-07-21 18:59:30 -0300

    estudos

--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@ require('dotenv').config({path:'variaveis.env'});
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
 const routes = require('./routes');
 const server = express();
+
+
 server.use(cors());
 server.use(bodyParser.urlencoded({
     extended:false
